4c4848669d76d3bd9e3edb9c402d738fe925b7c0
sbhr574 <sbhr574@gmail.com>
2023-05-01 11:44:40 +0530

    JS Operator

---
+++ Operators.js
@@ -0,0 +1,14 @@
+/**
+ * 1. Arithmetic
+ * 2. Assignment
+ * 3. Ternary
+ * 4. Comparison
+ * 5. Logical
+ * 6. Binary
+ */
+
+//1. Arithmetic Example
+
+let x = 100;
+let y = 23;
+console.log(100 * 23);
